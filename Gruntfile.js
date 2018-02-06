c02c46fd5d33c66c93b1c7d585a4796f231ba56c
Eyas Ranjous <eyas.ranjous@gmail.com>
2018-02-05 21:48:44 -0600

    update tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,39 @@
-module.exports = function(grunt) {
+'use strict';
+
+module.exports = function (grunt) {
 
     grunt.initConfig({
+
         jshint: {
             options: {
                 reporter: require('jshint-stylish'),
                 jshintrc: '.jshintrc'
             },
-            files: ['./lib/**/*.js', './test/*.spec.js']
+            files: ['./lib/*.js', './test/*.spec.js', './*.js']
         },
 
-         mochaTest: {
-            files: ['./test/**/*.spec.js']
+        mochaTest: {
+            files: ['./test/*.spec.js']
         },
+
+        mocha_istanbul: {
+            coverage: {
+                src: 'test',
+                options: {
+                    mask: '**/*.spec.js'
+                }
+            }
+        }
+
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
 
     grunt.registerTask('lint', ['jshint']);
     grunt.registerTask('test', ['mochaTest']);
-    grunt.registerTask('default', ['lint', 'test']);
-}
+    grunt.registerTask('coverage', ['mocha_istanbul']);
+    grunt.registerTask('build', ['lint', 'coverage']);
+};
